af1f2ef38ec7f38324d6e3c7b833f0a76b8038ab
Alejandro Vicente Grabovetsky <a.vicente.grab@gmail.com>
2012-05-22 20:24:54 +0200

    Fixed helper stream function

--- aa_engine/aas_findstream.m
+++ aa_engine/aas_findstream.m
@@ -5,14 +5,18 @@ function FNs = aas_findstream(aap, streamString, varargin)
 
 FNs = [];
 for Sind=1:length(aap.tasklist.currenttask.inputstreams.stream)
-    if ~isempty(strfind(aap.tasklist.currenttask.inputstreams.stream{Sind}, streamString))
-        switch (nargin)
-            case 2
-                FNs = aas_getfiles_bystream(aap, aap.tasklist.currenttask.inputstreams.stream{Sind});
-            case 3
-                FNs = aas_getfiles_bystream(aap,varargin{1}, aap.tasklist.currenttask.inputstreams.stream{Sind});
-            case 4
-                FNs = aas_getfiles_bystream(aap,varargin{1}, varargin{2}, aap.tasklist.currenttask.inputstreams.stream{Sind});
+    if isempty(FNs)
+        if ~isempty(strfind(aap.tasklist.currenttask.inputstreams.stream{Sind}, streamString))
+            switch (nargin)
+                case 2
+                    FNs = aas_getfiles_bystream(aap, aap.tasklist.currenttask.inputstreams.stream{Sind});
+                case 3
+                    FNs = aas_getfiles_bystream(aap,varargin{1}, aap.tasklist.currenttask.inputstreams.stream{Sind});
+                case 4
+                    FNs = aas_getfiles_bystream(aap,varargin{1}, varargin{2}, aap.tasklist.currenttask.inputstreams.stream{Sind});
+            end
         end
+    else
+        aas_log(aap, true, 'Several streams to that name found, not sure what to do!');
     end
 end
