ad6c692a785fe2f93423f2f6d0354f6677798c02
Johan Carlin <johan.carlin@gmail.com>
2012-10-11 14:07:14 +0100

    copy parameters in struct arr

--- aa_engine/aas_copyparameters.m
+++ aa_engine/aas_copyparameters.m
@@ -4,7 +4,22 @@ function destaap=aas_copyparameters(srcaap,destaap,nme)
 fn=fieldnames(srcaap);
 for fnind=1:length(fn)
     if (isstruct(srcaap.(fn{fnind})))
-        destaap.(fn{fnind})=aas_copyparameters(srcaap.(fn{fnind}),destaap.(fn{fnind}),[nme '.' fn{fnind}]);
+        % handle structure array parameters
+        destlen = length(destaap.(fn{fnind}));
+        srclen = length(srcaap.(fn{fnind}));
+        % if default array already had a length > srclen something is
+        % probably wrong
+        assert(destlen<=srclen,'mismatched struct arrays')
+        % upcast destlen to srclen
+        if destlen<srclen
+            destaap.(fn{fnind}) = destaap.(fn{fnind})(ones(1,srclen));
+        end
+        % iterate over struct array entries
+        for n = 1:length(srcaap.(fn{fnind}))
+            destaap.(fn{fnind})(n) = aas_copyparameters(...
+                srcaap.(fn{fnind})(n),destaap.(fn{fnind})(n),...
+                [nme '.' fn{fnind}]);
+        end
     else
         if (~isfield(destaap,fn{fnind}))
             aas_log(srcaap,true,sprintf('Error when copying extra parameters, field %s is present in %s of extraparameters.aap  but not in normal aap structure',fn{fnind},nme));
