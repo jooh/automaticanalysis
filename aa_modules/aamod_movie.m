c23bbb19b2b4f7704e3afbdb522a585bdd426ae7
Johan Carlin <johan.carlin@gmail.com>
2012-11-27 11:43:48 +0000

    now makes a single movie across sessions for easier checking of alignment

--- aa_modules/aamod_movie.m
+++ aa_modules/aamod_movie.m
@@ -1,8 +1,8 @@
 % Make a movie of an epi time series
-% [aap,resp]=aamod_movie(aap,task,i,j)
+% [aap,resp]=aamod_movie(aap,task,subj)
 % J Carlin 20120911
 
-function [aap,resp]=aamod_movie(aap,task,i,j)
+function [aap,resp]=aamod_movie(aap,task,subj)
 
 resp='';
 
@@ -10,17 +10,24 @@ switch task
     case 'description'
         resp='Run aamod_movie';
     case 'summary'
-        resp='Make EPI movies\n';
+        resp='Make EPI movie\n';
     case 'doit'
-        for j = aap.acq_details.selected_sessions
-            sesspath=aas_getsesspath(aap,i,j);
+        % now across sessions
+        V = [];
+        for sess = aap.acq_details.selected_sessions
+            sesspath=aas_getsesspath(aap,subj,sess);
             aas_makedir(aap,sesspath);
-            outfile = fullfile(sesspath,'diagnostic_aamod_movie.avi');
             % get files in this directory
-            imgs=aas_getimages_bystream(aap,i,j,'epi');
-            V = spm_vol(imgs);
-            volumes2movie(V,outfile,aap.tasklist.currenttask.settings.fps);
+            imgs=aas_getimages_bystream(aap,subj,sess,'epi');
+            if isempty(V)
+                V = spm_vol(imgs);
+            else
+                V = [V; spm_vol(imgs)];
+            end
         end
+        subjpath = aas_getsubjpath(aap,subj);
+        outfile = fullfile(subjpath,'diagnostic_aamod_movie.avi');
+        volumes2movie(V,outfile,aap.tasklist.currenttask.settings.fps);
     case 'checkrequirements'
         
     otherwise
