c87986373fb4edbaa0e2a07d48de6f8103ed70f2
Johan Carlin <johan.carlin@gmail.com>
2013-02-13 08:50:50 +0000

    debugged, moved spm convert to separate function

--- aa_modules/aamod_firstlevel_glmdenoise.m
+++ aa_modules/aamod_firstlevel_glmdenoise.m
@@ -9,7 +9,7 @@ switch task
         resp='subject';   % this module needs to be run once per subject
         
     case 'description'
-        resp='construct EPI and design pilab Volumes';
+        resp='run GLMDenoise';
         
     case 'summary'
         
@@ -19,66 +19,21 @@ switch task
         % model (convolved design matrix, so after aamod_firstlevel_design)
         spmpath = aas_getfiles_bystream(aap,subj,'firstlevel_spm');
         SPM = loadbetter(spmpath);
-        % epi data is loaded implicitly by reading off SPM
-        nchunks = length(SPM.nscan);
-
-        % get condition names and concatenate runs
-        names = arrayfun(@(x)x.name{1},SPM.Sess(1).U,...
-            'uniformoutput',false);
-        assert(issorted(names),['conditions should be sorted to ' ...
-            'avoid scrambling condition order']);
-        % take out conditions of no interest
-        ts = aap.tasklist.currenttask.settings;
-        [validnames,inds] = setdiff(names,ts.ignorelabels);
-        ncon = length(validnames);
-
-        % TODO: support for covariates (opt.extraregressors). But I suspect
-        % most covariates we can imagine would be subsumed by the noise PCs
-        % anyway.
-
-        % convert to GLMdenoise format
-        design = cell(1,nchunks);
-        epi = cell(1,nchunks);
         frameperiod = SPM.xY.RT;
-        interp = [];
-        dur = [];
-        for chunk = 1:nchunks
-            fprintf('converting session %d\n',chunk);
-            vols = SPM.xY.P(SPM.Sess(chunk).row,:);
-            % TODO: I suspect we will need to use spm_sample_vol to read
-            % masked brain here. But we should first assess performance
-            % with unmasked.
-            epi{chunk} = single(spm_read_vols(spm_vol(vols)));
-            % convert onsets in seconds to binary design matrix
-            design{chunk} = zeros(SPM.nscan(chunk),ncon);
-            % add conditions of interest
-            for con = 1:ncon
-                conind = inds(con);
-                assert(strcmp(names{con},...
-                    SPM.Sess(chunk).U(conind).name{1}),...
-                    'regressor order must be identical across runs');
-                % convert onsets to scans (time0 == volume 1 and so on)
-                scantime = 1 + SPM.Sess(chunk).U(conind).ons / frameperiod;
-                ons = round(scantime);
-                % track interpolation in s
-                interp = [interp abs(scantime-ons)*frameperiod];
-                assert(all(ons>0 & ons<SPM.nscan(chunk)),...
-                    'onsets outside of session scan');
-                if isempty(dur)
-                    dur = SPM.Sess(chunk).U(conind).dur;
-                else
-                    assert(all(dur == SPM.Sess(chunk).U(conind).dur),...
-                        'all events must have the same duration');
-                end
-                design{chunk}(ons,con) = 1;
-            end
-            assert(rank(design{chunk})==size(design{chunk},2),...
-                'rank deficient design matrix in chunk %d',chunk);
-        end
-        fprintf(['interpolated design matrix to scans ' ...
-            '(errors [s]: mean=%.2f, min=%.2f, max=%.2f)\n'],...
-            mean(interp),min(interp),max(interp));
-        clear SPM
+        % get mask - memory problems otherwise
+        % TO TEST: I suspect we'll now have to tweak the thresholds for
+        % detecting out of brain voxels etc.
+        % TODO: might make more sense to just load epivol and make an
+        % epivol2glmdenoise module.
+        mpath = aas_getfiles_bystream(aap,subj,'freesurfer_gmmask');
+        V = spm_vol(mpath(2,:));
+        mask = spm_read_vols(V) > 0;
+
+        % pass off to new independent function
+        ts = aap.tasklist.currenttask.settings;
+        % converts and gets rid of NaN / 0 voxels.
+        [epi,design,dur,mask,names] = spm2glmdenoise(SPM,mask,...
+          ts.ignorelabels,strcmp(ts.hrfmodel,'assumeconvolved'));
 
         % Kendrick's empirical HRFs look very noisy for short stimuli (<3
         % s) and don't work at all for duration 0 (impulse response). In
@@ -89,28 +44,75 @@ switch task
         % longer durations.
         % (note that the duration input is basically redundant - Kendrick's
         % code only uses the duration for creating the HRF so when this is
-        % given (e.g. when 'optimize' and 'hrfknobs' gives an HRF)
-        if dur==0 && isempty(ts.hrfknobs)
+        % given (e.g. when 'optimize' and 'hrfknobs' gives an HRF),
+        % duration is not used.
+        if dur==0 && isempty(ts.hrfknobs) && ~strcmp(ts.hrfmodel,'assumeconvolved')
             fprintf('event duration 0 so using spm HRF\n');
             ts.hrfknobs = normalizemax(spm_hrf(frameperiod));
         end
 
+        if ~isfield(ts.opt,'brainmask') || isempty(ts.opt.brainmask)
+            ts.opt.brainmask = mask;
+        end
+
         % fit GLMdenoise model
-        outdir = fullfile(aas_getsubjpath(aap,subj),'glmdenoise');
+        subdir = aas_getsubjpath(aap,subj);
+        outdir = fullfile(subdir,'glmdenoise');
         mkdirifneeded(outdir);
         figdir = fullfile(outdir,'diagnostic_figures');
         mkdirifneeded(figdir);
         [results,denoisedepi] = GLMdenoisedata(design,epi,dur,frameperiod,...
             ts.hrfmodel,ts.hrfknobs,ts.opt,figdir);
+        % add names field
+        results.regnames = names;
+        % recreate 'bright' logical mask marking voxel exceeding intensity
+        % threshold
+        results.bright = results.meanvol(:) > prctile(results.meanvol,...
+          results.inputs.opt.brainthresh(1) * results.inputs.opt.brainthresh(2));
 
-        % save and describe
+        % save and describe standard outputs
         outpath_results = fullfile(outdir,'results.mat');
-        save(outpath_results,'results','-v7');
+        % need 7.3 flag since this may be >2GB
+        save(outpath_results,'results','-v7.3');
         aap=aas_desc_outputs(aap,subj,'glmdenoise_results',outpath_results);
         outpath_epi = fullfile(outdir,'denoisedepi.mat');
-        save(outpath_epi,'denoisedepi','-v7');
+        save(outpath_epi,'denoisedepi','-v7.3');
         aap=aas_desc_outputs(aap,subj,'glmdenoise_epi',outpath_epi);
 
+        % update mask
+        V.fname = mpath(2,:);
+        spm_write_vol(V,mask);
+        aap=aas_desc_outputs(aap,subj,'freesurfer_gmmask',mpath);
+
+        % also write out a few diagnostic volumes
+        r2 = double(mask);
+        r2(mask) = results.R2;
+        outpath = fullfile(outdir,'R2.nii');
+        V.fname = outpath;
+        V.dt = [spm_type('float32') spm_platform('bigend')];
+        spm_write_vol(V,r2);
+        aap=aas_desc_outputs(aap,subj,'glmdenoise_diagnostic_r2',outpath);
+
+        snr = double(mask);
+        snr(mask) = results.SNR;
+        outpath = fullfile(outdir,'SNR.nii');
+        V.fname = outpath;
+        spm_write_vol(V,snr);
+        aap=aas_desc_outputs(aap,subj,'glmdenoise_diagnostic_snr',outpath);
+
+        noisepool = mask;
+        noisepool(mask) = results.noisepool;
+        outpath = fullfile(outdir,'noisepool.nii');
+        V.fname = outpath;
+        spm_write_vol(V,noisepool);
+        aap=aas_desc_outputs(aap,subj,'glmdenoise_diagnostic_noisepool',outpath);
+
+        bright = mask;
+        noisepool(mask) = results.bright;
+        outpath = fullfile(outdir,'bright.nii');
+        V.fname = outpath;
+        spm_write_vol(V,bright);
+        aap=aas_desc_outputs(aap,subj,'glmdenoise_diagnostic_noisepool',outpath);
     case 'checkrequirements'
         
     otherwise
