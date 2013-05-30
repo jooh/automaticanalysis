10373c158d1eaad8c4d5ad969f64c5bdf48a99f1
Johan Carlin <johan.carlin@gmail.com>
2013-05-30 11:50:16 +0100

    added spikes output from diffana

--- aa_modules/aamod_tsdiffana.m
+++ aa_modules/aamod_tsdiffana.m
@@ -27,13 +27,51 @@ switch task
         imgs = aas_getimages_bystream(aap,subj,sess,'epi');
         
         tsdiffana(imgs,0);
-        
+        outpath = fullfile(Spth,'timediff.mat');
+
         % Now produce graphical check
         try figure(spm_figure('FindWin', 'Graphics')); catch; figure(1); end;
+
+        ts = aap.tasklist.currenttask.settings;
+        spikes = [];
+        spikeout = fullfile(Spth,'spikes.mat');
+        spikethreshout = fullfile(Spth,'spikethresh.mat');
+        % any to handle string inputs
+        if ~any(isinf(ts.spikethreshold))
+            % get the axes for the scaled slice-to-slice variance
+            subplot(4,1,1);
+            if strcmp(ts.spikethreshold,'manual')
+                if exist(spikethreshout,'file')
+                    spikethresh = load(spikethreshout);
+                    spikethresh = spikethresh.spikethresh;
+                    fprintf('loaded existing threshold from %s\n',...
+                        spikethreshout);
+                else
+                    fprintf(['Use the mouse to select a scaled '...
+                        'variance threshold for spike identification.\n'])
+                    [junk,spikethresh] = ginput(1);
+                end
+                fprintf('spike threshold = %.2f\n',spikethresh);
+            end
+            % add thresh to plot
+            hold on
+            plot(xlim,[spikethresh spikethresh],'r:');
+            % vector of spikes
+            td = load(outpath);
+            % scale similar to plot
+            td = td.td / mean(td.globals);
+            % +1 since this is a difference measure - so if 1:2 is small
+            % and 2:3 is big we want to flag 3, not 2.
+            spikes = find(td>spikethresh) + 1;
+            fprintf('found %d spikes\n',length(spikes));
+            save(spikethreshout,'spikethresh');
+        end
         print('-djpeg','-r150',fullfile(Spth,'diagnostic_aamod_tsdiffana'));
+        save(spikeout,'spikes');
         
         % Save the time differences
-        aap = aas_desc_outputs(aap,subj,sess, 'tsdiffana', fullfile(Spth, 'timediff.mat'));
+        aap = aas_desc_outputs(aap,subj,sess, 'tsdiffana', outpath);
+        aap = aas_desc_outputs(aap,subj,sess, 'spikes', spikeout);
         
     case 'checkrequirements'
         
