ea2a379441b026067bd40640b65c740a24506593
Johan Carlin <johan.carlin@gmail.com>
2013-06-14 14:26:09 +0100

    import roitools ROIs

---
+++ aa_modules/aamod_pilab_importrois.m
@@ -0,0 +1,36 @@
+% Import roitools ROIs pilab analysis 
+function [aap,resp]=aamod_pilab_importrois(aap,task,subj)
+
+resp = '';
+
+switch task
+    case 'report'
+
+    case 'doit'
+        % find subject name
+        subname = aap.acq_details.subjects(subj).mriname;
+        roidir = fullfile(aap.tasklist.currenttask.settings.roiroot,...
+            subname);
+        assert(exist(roidir,'dir')~=0,'no roi dir found: %s',roidir);
+
+        if aap.tasklist.currenttask.settings.usesmoothrois
+            target = 'roivol.mat';
+        else
+            target = 'roivol_unsm.mat';
+        end
+        roivol = loadbetter(fullfile(roidir,target));
+
+        % intersect ROI with pilab mask
+        pipath = aas_getfiles_bystream(aap,subj,'pilab_volume');
+        pivol = loadbetter(pipath);
+        goodinds = find(roivol.mask & pivol.mask);
+        roivol = roivol(:,goodinds);
+
+        pidir = fileparts(pipath);
+        outpath = fullfile(pidir,'pilab_rois.mat');
+        save(outpath,'roivol');
+        aap = aas_desc_outputs(aap,subj,'pilab_rois',outpath);
+    case 'checkrequirements'
+    otherwise
+        aas_log(aap,1,sprintf('Unknown task %s',task));
+end
