b1a2117a03073d1f00e5542d38c4814a8d3cb100
Johan Carlin <johan.carlin@gmail.com>
2013-10-24 10:54:18 +0100

    visualisation modules for single subject and RFX rsa results.

---
+++ aa_modules/aamod_pilab_rsa_visualisation.m
@@ -0,0 +1,33 @@
+% visualise rsa results. 
+%
+% [aap,resp]=aamod_pilab_rsa_visualisation(aap,task,subj)
+function [aap,resp]=aamod_pilab_rsa_visualisation(aap,task,subj)
+
+resp='';
+
+switch task
+    case 'doit'
+        % get the results
+        res = loadbetter(aas_getfiles_bystream(aap,subj,...
+            'pilab_rsa_r'));
+
+        ts = aap.tasklist.currenttask.settings;
+        % prepare output
+        pidir = fullfile(aas_getsubjpath(aap,subj),'pilab');
+        figdir = fullfile(pidir,'figures');
+
+        % hey, we can use this again!
+        plot_lindisc(figdir,res,'mtarget',ts.mtarget,'errtarget',...
+            ts.errtarget,'ptarget',ts.ptarget,'mlabel',ts.mlabel,...
+            'errlabel',ts.errlabel,'pthresh',ts.pthresh);
+        if ~isempty(ts.pluginpath)
+            feval(ts.pluginpath,figdir,res,'mtarget',ts.mtarget,...
+                'errtarget',ts.errtarget,'ptarget',ts.ptarget,...
+                'mlabel',ts.mlabel,'errlabel',ts.errlabel,...
+                'pthresh',ts.pthresh);
+        end
+    case 'checkrequirements'
+        
+    otherwise
+        aas_log(aap,1,sprintf('Unknown task %s',task));
+end
---
+++ aa_modules/aamod_pilab_rsa_visualisation_rfx.m
@@ -0,0 +1,33 @@
+% visualise RSA RFX results.
+%
+% [aap,resp]=aamod_pilab_rsa_visualisation_rfx(aap,task)
+function [aap,resp]=aamod_pilab_rsa_visualisation_rfx(aap,task)
+
+resp='';
+
+switch task
+    case 'doit'
+        % get the results
+        meanres = loadbetter(aas_getfiles_bystream(aap,'pilab_rsa_r_rfx'));
+        ts = aap.tasklist.currenttask.settings;
+
+        % prepare output
+        pidir = fullfile(aas_getstudypath(aap),'pilab');
+        figdir = fullfile(pidir,'figures');
+
+        plot_lindisc(figdir,meanres,'mtarget',ts.mtarget,'errtarget',...
+            ts.errtarget,'ptarget',ts.ptarget,'mlabel',ts.mlabel,...
+            'errlabel',ts.errlabel,'pthresh',ts.pthresh);
+        if ~isempty(ts.pluginpath)
+            % call on mean
+            feval(ts.pluginpath,figdir,meanres,'mtarget',ts.mtarget,...
+                'errtarget',ts.errtarget,'ptarget',ts.ptarget,...
+                'mlabel',ts.mlabel,'errlabel',ts.errlabel,...
+                'pthresh',ts.pthresh);
+        end
+
+    case 'checkrequirements'
+        
+    otherwise
+        aas_log(aap,1,sprintf('Unknown task %s',task));
+end
