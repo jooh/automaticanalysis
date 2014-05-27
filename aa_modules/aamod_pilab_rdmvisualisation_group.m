814d266b434c1b4fd616d57f416ec661c0caa266
Johan Carlin <johan.carlin@gmail.com>
2014-05-27 15:28:19 +0100

    updated to reflect changes in plotrdms_batch

--- aa_modules/aamod_pilab_rdmvisualisation_group.m
+++ aa_modules/aamod_pilab_rdmvisualisation_group.m
@@ -6,9 +6,8 @@ resp='';
 
 switch task
     case 'doit'
-        % get mean data RDM
-        vpath = aas_getfiles_bystream(aap,'pilab_data_rdms_group_mean');
-        disvol = loadbetter(vpath);
+        % get RDMs
+        meanres = loadbetter(aas_getfiles_bystream(aap,'pilab_rdms_rfx'));
         % get stimuli (NB, we use subject 1's stimuli as an example)
         spath = aas_getfiles_bystream(aap,1,'pilab_stimuli');
         stimuli = loadbetter(spath);
@@ -23,12 +22,18 @@ switch task
         ts = aap.tasklist.currenttask.settings;
         % quick check to avoid accidentally computing this on a full
         % searchlight disvol
-        assert(disvol.nfeatures <= ts.maxn,...
+        assert(size(meanres.mean,2) <= ts.maxn,...
             'number of ROIs exceed maxn (%d)',ts.maxn);
 
-        plotrdms_batch(disvol,stimuli,'figdir',figdir,...
-            'cmap',ts.cmap,'nrows',ts.nrows,'alphacolor',ts.alphacolor,...
-            'mdstimsize',ts.mdstimsize);
+        if ~isempty(ts.pluginpath)
+            feval(ts.pluginpath,meanres,stimuli,figdir,ts);
+        end
+
+        plotrdms_batch('data',meanres.(ts.target),'roinames',...
+            meanres.cols_roi,'labels',stimuli,'figdir',figdir,...
+            'cmap',ts.cmap,'nrows',ts.nrows,'gridlines',ts.gridlines,...
+            'gridcolor',ts.gridcolor,'ranktransform',ts.ranktransform);
+
 
     case 'checkrequirements'
         
