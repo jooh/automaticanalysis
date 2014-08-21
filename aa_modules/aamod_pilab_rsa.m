ecba599728e2b84e77d4fa6a64f35ff65cfe6093
Johan Carlin <johan.carlin@gmail.com>
2014-08-21 15:23:07 +0100

    more flexible input behaviours to support e.g. multirsa

--- aa_modules/aamod_pilab_rsa.m
+++ aa_modules/aamod_pilab_rsa.m
@@ -7,18 +7,28 @@ resp='';
 
 switch task
     case 'doit'
+        ts = aap.tasklist.currenttask.settings;
+
         % get data RDMs
-        vpath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_mean');
-        disvol = loadbetter(vpath);
+        switch ts.input
+            case 'mean'
+                vpath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_mean');
+                disvol = {loadbetter(vpath)};
+            case 'sess'
+                vpath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_sess');
+                disvol = arrayfun(@(x)loadbetter(vpath(x,:)),...
+                    1:size(vpath,1),'uniformoutput',0);
+            otherwise
+                error('unknown input: %s',ts.input);
+        end
 
         % predictor RDMs
-        ts = aap.tasklist.currenttask.settings;
         if isempty(ts.predictorfun)
             predictpath = aas_getfiles_bystream(aap,subj,...
                 'pilab_rsapredictors');
             predictors = loadbetter(predictpath);
         else
-            fprintf('using custom predictor RDMs from %s\n',...
+            logstr('using custom predictor RDMs from %s\n',...
                 ts.predictorfun);
             predictors = feval(ts.predictorfun);
         end
@@ -31,18 +41,7 @@ switch task
             predictors = predictors(ts.selectpredictorinds);
         end
         predictors(ts.removepredictorinds) = [];
-        ncon = length(predictors);
         
-        % this is a bit hacky but sometimes it is advantageous to drop the
-        % precision before carting off to roidata_rsa
-        if ~isempty(ts.setclass)
-            fprintf('setting data and predictors to %s\n',ts.setclass);
-            disvol.data = feval(ts.setclass,disvol.data);
-            for p = 1:ncon
-                predictors(p).RDM = feval(ts.setclass,predictors(p).RDM);
-            end
-        end
-
         % partial rho support. The beauty of this approach is that
         % roidata_rsa does not need to know that this is the analysis we're
         % running...
@@ -58,31 +57,48 @@ switch task
             % doesn't make sense to fit these anymore
             predictors(partialind) = [];
         end
+        % multirsa and related supports
+        if ~isempty(ts.fitmethodpredictors)
+            [~,fmind] = intersect({predictors.name},...
+                ts.fitmethodpredictors);
+            assert(~isempty(fmind),'no match for fitmethodpredictors');
+            assert(isempty(ts.fitmethodargs),...
+                'fitmethodargs must be empty for this mode');
+            assert(isempty(ts.splitrsaclass),...
+                'no split RSA support for fitmethodargs at present');
+            ts.fitmethodargs = {predictors(fmind)};
+            % unlike partialpredictors we still keep the original predictor
+            logstr('using %s as fitmethodpredictors\n',...
+                ts.fitmethodpredictors);
+        end
         ncon = length(predictors);
 
-        fprintf('running roidata_rsa with %d predictors and %d rois\n',...
-            ncon,disvol.nfeatures);
+        logstr('running roidata_rsa with %d predictors and %d rois\n',...
+            ncon,disvol{1}.nfeatures);
         tic;
         [res,nulldist,bootdist] = roidata_rsa(disvol,predictors,'nperm',...
             ts.nperm,'nboot',ts.nboot,'rsaclass',ts.rsaclass,...
             'rsaclassargs',ts.rsaclassargs,...
             'splitrsaclass',ts.splitrsaclass,...
-            'splitrsaclassargs',ts.splitrsaclassargs);
-        fprintf('finished in %s.\n',seconds2str(toc));
+            'splitrsaclassargs',ts.splitrsaclassargs,...
+            'fitmethod',ts.fitmethod,...
+            'fitmethodargs',ts.fitmethodargs,...
+            'customfun',ts.customfun);
+        logstr('finished in %s.\n',seconds2str(toc));
 
         % save and describe
         pidir = fullfile(aas_getsubjpath(aap,subj),'pilab');
 
         if isempty(ts.outputmode)
-            switch class(disvol)
+            switch class(disvol{1})
                 case 'BaseVolume'
                     ts.outputmode = 'roi';
-                    fprintf('auto-set outputmode to roi\n');
+                    logstr('auto-set outputmode to roi\n');
                 case 'MriVolume'
                     ts.outputmode = 'searchlight';
-                    fprintf('auto-set outputmode to searchlight\n');
+                    logstr('auto-set outputmode to searchlight\n');
                 otherwise
-                    error('unknown outputmode for class %s',class(disvol));
+                    error('unknown outputmode for class %s',class(disvol{1}));
             end
         end
 
@@ -103,17 +119,17 @@ switch task
                         '_');
                     rpaths{c} = fullfile(pidir,sprintf('rsa_r_%s.nii',...
                         outname));
-                    disvol.data2file(res.r(c,:),rpaths{c});
+                    disvol{1}.data2file(res.r(c,:),rpaths{c});
                     % also write out p maps (these are not logged as an
                     % output stream though)
                     if ts.nperm > 1
                         ppath = fullfile(pidir,sprintf(...
                             'rsa_-log10p_%s.nii',outname));
-                        disvol.data2file(-log10(res.pperm(c,:)),ppath);
+                        disvol{1}.data2file(-log10(res.pperm(c,:)),ppath);
                         pfwe = permpfwe(squeeze(res.nulldist(c,:,:))');
                         pfwepath = fullfile(pidir,sprintf(...
                             'rsa_-log10pFWE_%s.nii',outname));
-                        disvol.data2file(-log10(pfwe),pfwepath);
+                        disvol{1}.data2file(-log10(pfwe),pfwepath);
                     end
                 end
                 aap=aas_desc_outputs(aap,subj,'pilab_rsa_r',rpaths);
