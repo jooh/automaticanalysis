a17681e5816660f6c9134bf778a366a02ad21c25
Johan Carlin <johan.carlin@gmail.com>
2014-08-21 15:20:41 +0100

    no more figures (use separate rdm_visualisation modules if you want this)

--- aa_modules/aamod_pilab_importrsapredictors.m
+++ aa_modules/aamod_pilab_importrsapredictors.m
@@ -37,35 +37,7 @@ switch task
             end
         end
         save(outpath,'rdms');
-
-        figdir = fullfile(pidir,'figures');
-        mkdirifneeded(figdir);
         aap = aas_desc_outputs(aap,subj,'pilab_rsapredictors',outpath);
-
-        % Also make quick diagnostic figures
-        for r = 1:length(rdms)
-            % base rdm
-            F = figure;
-            rdmplot(gca,rdms(r),'labels',{stimuli.image},'nrows',...
-                ts.nrows);
-            printstandard(fullfile(figdir,['predictor_rdm_' rdms(r).name]));
-            close(F);
-            im = intensity2rgb(rdms(r).RDM,cmap_bwr);
-            imwrite(im,fullfile(figdir,sprintf('predictor_rawrdm_%s.png',...
-                rdms(r).name)),'PNG');
-        end
-        % show second-order RSM
-        rsm = corr(asrdmvec(rdms),'rows','pairwise','type','spearman');
-        F = figurebetter([],'huge',1);
-        subplot(1,2,1);
-        [ax,intmap,cmap] = rdmplot(gca,rsm,'limits',[-1 1]);
-        axis(ax,'on');
-        set(ax,'yticklabel',stripbadcharacters({rdms.name},' '),...
-            'xticklabel',[]);
-        sax = subplot(1,2,2);
-        c = colorbarbetter(sax,intmap,cmap,'label','spearman rho');
-        printstandard(fullfile(figdir,'predictor_so_rsm'));
-        close(F);
     case 'checkrequirements'
     otherwise
         aas_log(aap,1,sprintf('Unknown task %s',task));
