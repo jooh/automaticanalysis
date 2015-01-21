3b929109f9be6333cbb0e94d079c1be17e09926d
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 18:30:29 +0000

    use new functions, support roidata_rsa improvements

--- aa_modules/aamod_pilab_rsa.m
+++ aa_modules/aamod_pilab_rsa.m
@@ -14,12 +14,23 @@ switch task
             case 'mean'
                 vpath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_mean');
                 disvol = {loadbetter(vpath)};
+                npath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_nulldist');
+                nullrdm = loadbetter(npath);
             case 'sess'
                 vpath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_sess');
                 disvol = arrayfun(@(x)loadbetter(vpath(x,:)),...
                     1:size(vpath,1),'uniformoutput',0);
+                npath = aas_getfiles_bystream(aap,subj,'pilab_data_rdms_sessnulldist');
+                nullrdm = arrayfun(@(x)loadbetter(npath(x,:)),...
+                    1:size(npath,1),'uniformoutput',0);
             otherwise
                 error('unknown input: %s',ts.input);
+        end
+
+        if ts.nperm > 1 && strcmp(ts.permtype,'rsa')
+            % remove nullrdm to prevent roidata_rsa from running
+            % RDM-based permutation test
+            nullrdm = [];
         end
 
         % predictor RDMs
@@ -42,9 +53,6 @@ switch task
         end
         predictors(ts.removepredictorinds) = [];
         
-        % partial rho support. The beauty of this approach is that
-        % roidata_rsa does not need to know that this is the analysis we're
-        % running...
         if ~isempty(ts.partialpredictors)
             [~,partialind] = intersect({predictors.name},...
                 ts.partialpredictors);
@@ -73,17 +81,19 @@ switch task
         end
         ncon = length(predictors);
 
-        logstr('running roidata_rsa with %d predictors and %d rois\n',...
-            ncon,disvol{1}.nfeatures);
-        tic;
-        [res,nulldist,bootdist] = roidata_rsa(disvol,predictors,'nperm',...
-            ts.nperm,'nboot',ts.nboot,'rsaclass',ts.rsaclass,...
-            'rsaclassargs',ts.rsaclassargs,...
+        logstr('running %s/%s with %d predictors and %d rois\n',...
+            ts.rsaclass, ts.splitrsaclass, ncon,disvol{1}.nfeatures);
+        args = {'nperm',ts.nperm,'nboot',ts.nboot,...
+            'rsaclass',ts.rsaclass,'rsaclassargs',ts.rsaclassargs,...
             'splitrsaclass',ts.splitrsaclass,...
             'splitrsaclassargs',ts.splitrsaclassargs,...
-            'fitmethod',ts.fitmethod,...
-            'fitmethodargs',ts.fitmethodargs,...
-            'customfun',ts.customfun);
+            'fitmethod',ts.fitmethod,'fitmethodargs',ts.fitmethodargs,...
+            'customfun',ts.customfun,...
+            'bootmeth',ts.bootmeth,'bootprep',ts.bootprep,...
+            'permdis',nullrdm,'customfits',ts.customfits,...
+            'contrasts',ts.contrasts};
+        tic;
+        [res,nulldist,bootdist] = roidata_rsa(disvol,predictors,args{:});
         logstr('finished in %s.\n',seconds2str(toc));
 
         % save and describe
@@ -91,10 +101,10 @@ switch task
 
         if isempty(ts.outputmode)
             switch class(disvol{1})
-                case 'BaseVolume'
+                case 'Volume'
                     ts.outputmode = 'roi';
                     logstr('auto-set outputmode to roi\n');
-                case 'MriVolume'
+                case 'SPMVolume'
                     ts.outputmode = 'searchlight';
                     logstr('auto-set outputmode to searchlight\n');
                 otherwise
@@ -102,14 +112,24 @@ switch task
             end
         end
 
+        % save results as mat regardless
+        outpath_r = fullfile(pidir,'rsa_r.mat');
+        save(outpath_r,'res','-v7.3');
+        aap=aas_desc_outputs(aap,subj,'pilab_rsa_r',...
+            outpath_r);
+        outpath_null = fullfile(pidir,'rsa_nulldist.mat');
+        save(outpath_null,'nulldist','-v7.3');
+        aap=aas_desc_outputs(aap,subj,'pilab_rsa_nulldist',...
+            outpath_null);
+        outpath_boot = fullfile(pidir,'rsa_bootdist.mat');
+        save(outpath_boot,'bootdist','-v7.3');
+        aap=aas_desc_outputs(aap,subj,'pilab_rsa_bootdist',...
+            outpath_boot);
+
+        % maybe also write out niftis
         switch ts.outputmode
             case 'roi'
-                % save result as mat
-                outpath_r = fullfile(pidir,'rsa_r.mat');
-                save(outpath_r,'res');
-                aap=aas_desc_outputs(aap,subj,'pilab_rsa_r',...
-                    outpath_r);
-
+                % do nothing
             case 'searchlight'
                 ncon = size(res.r,1);
                 rpaths = cell(ncon,1);
@@ -117,36 +137,24 @@ switch task
                 for c = 1:ncon
                     outname =  stripbadcharacters(res.rows_contrast{c},...
                         '_');
-                    rpaths{c} = fullfile(pidir,sprintf('rsa_r_%s.nii',...
+                    rpaths = fullfile(pidir,sprintf('rsa_r_%s.nii',...
                         outname));
-                    disvol{1}.data2file(res.r(c,:),rpaths{c});
+                    disvol{1}.data2file(res.r(c,:),rpaths);
                     % also write out p maps (these are not logged as an
                     % output stream though)
-                    if ts.nperm > 1
+                    if isfield(res,'pperm')
                         ppath = fullfile(pidir,sprintf(...
                             'rsa_-log10p_%s.nii',outname));
                         disvol{1}.data2file(-log10(res.pperm(c,:)),ppath);
-                        pfwe = permpfwe(squeeze(res.nulldist(c,:,:))');
+                        pfwe = permpfwe(squeeze(nulldist.r(c,:,:))');
                         pfwepath = fullfile(pidir,sprintf(...
                             'rsa_-log10pFWE_%s.nii',outname));
                         disvol{1}.data2file(-log10(pfwe),pfwepath);
                     end
                 end
-                aap=aas_desc_outputs(aap,subj,'pilab_rsa_r',rpaths);
             otherwise
                 error('unknown outputmode: %s',ts.outputmode);
         end
-
-        % null and bootdists get saved as mats regardless
-        outpath_null = fullfile(pidir,'rsa_nulldist.mat');
-        save(outpath_null,'nulldist');
-        aap=aas_desc_outputs(aap,subj,'pilab_rsa_nulldist',...
-            outpath_null);
-
-        outpath_boot = fullfile(pidir,'rsa_bootdist.mat');
-        save(outpath_boot,'bootdist');
-        aap=aas_desc_outputs(aap,subj,'pilab_rsa_bootdist',...
-            outpath_boot);
 
     case 'checkrequirements'
         
