decabee955bb3394f08877cd1a3834f2a7abe0c7
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 18:29:00 +0000

    rowind and colind inputs, standard plots now optional

--- aa_modules/aamod_pilab_rdms_visualisation_group.m
+++ aa_modules/aamod_pilab_rdms_visualisation_group.m
@@ -11,44 +11,65 @@ switch task
         % get stimuli (NB, we use subject 1's stimuli as an example)
         spath = aas_getfiles_bystream(aap,1,'pilab_stimuli');
         stimuli = loadbetter(spath);
+        ts = aap.tasklist.currenttask.settings;
+        rowlabels = stimuli;
+        collabels = stimuli;
+        if ~isempty(ts.rowind)
+            if ischar(ts.rowind)
+                ts.rowind = eval(ts.rowind);
+            end
+            rowlabels = stimuli(ts.rowind);
+        end
+        if ~isempty(ts.colind)
+            if ischar(ts.colind)
+                ts.colind = eval(ts.colind);
+            end
+            collabels = stimuli(ts.colind);
+        end
+
         groupres = loadbetter(aas_getfiles_bystream(aap,'pilab_rdms_group'));
         
         % prepare output dirs
         pidir = fullfile(aas_getstudypath(aap),'pilab');
-        resdir = fullfile(pidir,'results');
-        mkdirifneeded(resdir);
-        figdir = fullfile(resdir,'figures');
+        figdir = fullfile(pidir,'figures');
         mkdirifneeded(figdir);
 
-        ts = aap.tasklist.currenttask.settings;
         % quick check to avoid accidentally computing this on a full
         % searchlight disvol
-        assert(size(meanres.mean,2) <= ts.maxn,...
+        assert(size(meanres.(ts.target),2) <= ts.maxn,...
             'number of ROIs exceed maxn (%d)',ts.maxn);
 
         if ~isempty(ts.pluginpath)
-            feval(ts.pluginpath,meanres,stimuli,figdir,ts);
-        end
-
-        plotrdms_batch('data',meanres.(ts.target),'roinames',...
-            meanres.cols_roi,'labels',stimuli,'figdir',figdir,...
-            'cmap',ts.cmap,'nrows',ts.nrows,'gridlines',ts.gridlines,...
-            'gridcolor',ts.gridcolor,'ranktransform',ts.ranktransform);
-
-        for roi = 1:numel(meanres.cols_roi)
-            roidata = squeeze(groupres.t(:,roi,:));
-            if ts.ranktransform==1
-                roidata = tiedrank(roidata);
+            if ~iscell(ts.pluginpath)
+                ts.pluginpath = {ts.pluginpath};
             end
-            % nb we omit the labels here because this figure has many
-            % panels and gets quite messy
-            F(roi) = rdmfig(roidata,meanres.z_subject,[],[],'labels',[],...
-                'cmap',ts.cmap,'gridlines',ts.gridlines,'gridcolor',...
-                ts.gridcolor);
-            set(F(roi),'name',['singles ' meanres.cols_roi{roi}]);
+            for n = 1:numel(ts.pluginpath)
+                feval(ts.pluginpath{n},meanres,groupres,stimuli,figdir,ts);
+            end
         end
-        printbyname(F,figdir);
-        close(F);
+
+        if ts.runstandardplots
+            plotrdms_batch('data',meanres.(ts.target),'roinames',...
+                meanres.cols_roi,'ylabels',rowlabels,'xlabels',collabels,...
+                'figdir',figdir,...
+                'cmap',ts.cmap,'nrows',ts.nrows,'gridlines',ts.gridlines,...
+                'gridcolor',ts.gridcolor,'ranktransform',ts.ranktransform);
+
+            for roi = 1:numel(meanres.cols_roi)
+                roidata = squeeze(groupres.t(:,roi,:));
+                if ts.ranktransform==1
+                    roidata = ranktrans(roidata);
+                end
+                % nb we omit the labels here because this figure has many
+                % panels and gets quite messy
+                F(roi) = rdmfig(roidata,meanres.z_subject,[],[],'labels',[],...
+                    'cmap',ts.cmap,'gridlines',ts.gridlines,'gridcolor',...
+                    ts.gridcolor);
+                set(F(roi),'name',['singles ' meanres.cols_roi{roi}]);
+            end
+            printbyname(F,figdir);
+            close(F);
+        end
 
     case 'checkrequirements'
         
