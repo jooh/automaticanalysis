5793d71fe1813ca816243813990e331d64ed9454
Johan Carlin <johan.carlin@gmail.com>
2015-08-25 12:18:24 +0100

    now wrapper for new preprocessvols function in pilab

--- aa_modules/aamod_pilab_preprocess.m
+++ aa_modules/aamod_pilab_preprocess.m
@@ -14,116 +14,13 @@ switch task
         epivol = loadbetter(epipath);
         ts = aap.tasklist.currenttask.settings;
 
-        if ts.matchn
-            nperchunk = arrayfun(@(c)sum(epivol.meta.samples.chunks==c),...
-                epivol.desc.samples.unique.chunks);
-            targetn = min(nperchunk);
-            if all(targetn == nperchunk);
-                logstr('all chunks have same number of samples\n');
-            else
-                logstr('matching nsamples to smallest chunk (%d)\n',...
-                    targetn);
-                % this might actually be a tad hairy. need to apply the
-                % same to design and epi obviously
-                goodsamp = false(epivol.nsamples,1);
-                for c = 1:epivol.desc.samples.nunique.chunks
-                    chunkind = find(epivol.meta.samples.chunks == ...
-                        epivol.desc.samples.unique.chunks(c));
-                    goodsamp(chunkind(1:targetn)) = true;
-                end
-                epivol = epivol(goodsamp,:);
-                designvol = designvol(goodsamp,:);
-                logstr('removed %d samples (%2.0f%% of total)\n',...
-                    sum(~goodsamp),100*sum(~goodsamp) / numel(goodsamp));
-            end
-        end
-
-        % de-trend config
-        if strcmp(ts.covariatedeg,'adaptive')
-            ts.covariatedeg = vol2covdeg(epivol);
-        end
-
-        % first high-pass trend removal
-        if ~isempty(ts.covariatedeg)
-            logstr('polynomial detrend (degree=%.0f)\n',ts.covariatedeg);
-            filterbychunk(epivol,'polydetrend',ts.covariatedeg);
-            filterbychunk(designvol,'polydetrend',ts.covariatedeg);
-        end
-
-        if ~isempty(ts.medianfilter) && ts.medianfilter
-            logstr('median filter (n=%.0f)\n',ts.medianfilter);
-            filterbychunk(epivol,'medianfilter',ts.medianfilter);
-            filterbychunk(designvol,'medianfilter',ts.medianfilter);
-        end
-
-        if ts.sgdetrend
-            logstr('Savitzky-Golay detrend (k=%.0f,f=%.0f)\n',...
-                ts.sgolayK,ts.sgolayF);
-            % insure double
-            epivol.data = double(epivol.data);
-            designvol.data = double(designvol.data);
-            sgdetrend(epivol,ts.sgolayK,ts.sgolayF);
-            sgdetrend(designvol,ts.sgolayK,ts.sgolayF);
-        end
-
-        if ts.zscore
-            logstr('Z-scoring samples\n')
-            filterbychunk(epivol,'zscore',[],1);
-            filterbychunk(designvol,'zscore',[],1);
-        end
-
-        % maybe deal with covariates
-        nlabels = length(designvol.desc.features.unique.labels);
-        if isempty(ts.targetlabels)
-            coninds = 1:nlabels;
-        else
-            coninds = find(strcmp(designvol.desc.features.unique.labels,...
-                ts.targetlabels));
-        end
-        if ~isempty(ts.ignorelabels)
-            ignoreinds = find(strcmp(designvol.desc.features.unique.labels,...
-                ts.ignorelabels));
-            coninds = setdiff(coninds,ignoreinds,'stable');
-        end
-        assert(~isempty(coninds),...
-            'found no labels matching targetlabels/ignorelabels %s/%s',...
-            ts.targetlabels,ts.ignorelabels);
-        if ~isequal(coninds,1:nlabels)
-            covariates = designvol.data(:,ignoreinds);
-            designvol = designvol(:,coninds);
-            % project out bad cons
-            if ~isempty(ignoreinds)
-                logstr('projecting out %d covariates\n',...
-                    size(covariates,2));
-                for c = 1:epivol.desc.samples.nunique.chunks
-                    chunkind = epivol.meta.samples.chunks == ...
-                        epivol.desc.samples.unique.chunks(c);
-                    assert(isequal(chunkind,...
-                        designvol.meta.samples.chunks == ...
-                        designvol.desc.samples.unique.chunks(c)),...
-                        'mismatched chunks in epivol and designvol');
-                    epivol.data(chunkind,:) = projectout(...
-                        epivol.data(chunkind,:),covariates(chunkind,:));
-                    designvol.data(chunkind,:) = projectout(...
-                        designvol.data(chunkind,:),covariates(chunkind,:));
-                end
-            end
-        end
-
-        % now set class last - so maximal precision for pre-processing
-        if ~isempty(ts.setclass)
-            logstr('setting data to %s\n',ts.setclass);
-            epivol.data = feval(ts.setclass,epivol.data);
-            designvol.data = feval(ts.setclass,designvol.data);
-        end
-
-        if ~isempty(ts.resortind)
-            if ischar(ts.resortind)
-                ts.resortind = feval(ts.resortind,designvol.nfeatures);
-            end
-            logstr('resorting regressors in designvol.\n');
-            designvol = designvol(:,ts.resortind);
-        end
+        [epivol,designvol] = preprocessvols(epivol,designvol,...
+            'matchn',ts.matchn,'covariatedeg',ts.covariatedeg,...
+            'domedianfilter',ts.medianfilter,'sgdetrend',ts.sgdetrend,...
+            'sgolayK',ts.sgolayK,'sgolayF',ts.sgolayF,...
+            'dozscore',ts.zscore,'targetlabels',ts.targetlabels,...
+            'ignorelabels',ts.ignorelabels,'setclass',ts.setclass,...
+            'resortind',ts.resortind);
 
         % output
         logstr('saving...\n');
